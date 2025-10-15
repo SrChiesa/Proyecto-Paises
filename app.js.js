e18c43c8427a61e69fe0512345046c9c0384ad30
Chiesa Matias <chiesa.matiasgerman@iesconcepcion.edu.ar>
2025-10-15 18:47:22 -0300

    Primer Commit

---
+++ app.js.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+const nombrePais = prompt('Ingrese el nombre de un país: ');
+
+fetch('https://restcountries.com/v3.1/name/argentina')
+  .then(res => res.json())
+  .then(data => {
+    const pais = data[0];
+    console.log('--- Información de Argentina ---');
+    console.log('Nombre:', pais.name.common);
+    console.log('Capital:', pais.capital[0]);
+    console.log('Región:', pais.region);
+  });
